bd5ce645e7c6cab261973f28c9ebac822d716ff1
Joshua Becnel <jcbecnel@gmail.com>
2025-03-05 08:16:23 -0600

    removed extra to_ascii_uppercase

--- src/clauses/insert/data_conversion/datetime.rs
+++ src/clauses/insert/data_conversion/datetime.rs
@@ -8,91 +8,91 @@ pub fn datetime_conversion(input: &mut String) -> Result<NaiveDateTime, String> 
         .to_ascii_uppercase();
 
     // mm/dd/yyyy hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%m-%d-%Y %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%Y %I:%M:%S %p") {
         return Ok(date_time)
     }
     // dd/mm/yyyy hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%d-%m-%Y %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%Y %I:%M:%S %p") {
         return Ok(date_time)
     }
     // yyyy/mm/dd hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%Y-%m-%d %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%m-%d %I:%M:%S %p") {
         return Ok(date_time)
     }
     // yyyy/dd/mm hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%Y-%d-%m %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%d-%m %I:%M:%S %p") {
         return Ok(date_time)
     }
     // mm/dd/yyyy hh:mm:ss
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%m-%d-%Y %H:%M:%S") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%Y %H:%M:%S") {
         return Ok(date_time)
     }
     // dd/mm/yyyy hh:mm:ss
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%d-%m-%Y %H:%M:%S") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%Y %H:%M:%S") {
         return Ok(date_time)
     }
     // yyyy/mm/dd hh:mm:ss
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%Y-%m-%d %H:%M:%S") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%m-%d %H:%M:%S") {
         return Ok(date_time)
     }
     // mm/dd/yyyy hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%m-%d-%Y %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%Y %I:%M %p") {
         return Ok(date_time)
     }
     // dd/mm/yyyy hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%d-%m-%Y %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%Y %I:%M %p") {
         return Ok(date_time)
     }
     // yyyy/mm/dd hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%Y-%m-%d %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%m-%d %I:%M %p") {
         return Ok(date_time)
     }
     // yyyy/dd/mm hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%Y-%d-%m %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%d-%m %I:%M %p") {
         return Ok(date_time)
     }
     // mm/dd/yy hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%m-%d-%y %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%y %I:%M:%S %p") {
         return Ok(date_time)
     }
     // dd/mm/yy hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%d-%m-%y %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%y %I:%M:%S %p") {
         return Ok(date_time)
     }
     // yy/mm/dd hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%y-%m-%d %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%y-%m-%d %I:%M:%S %p") {
         return Ok(date_time)
     }
     // yy/dd/mm hh:mm:ss AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%y-%d-%m %I:%M:%S %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%y-%d-%m %I:%M:%S %p") {
         return Ok(date_time)
     }
     // mm/dd/yy hh:mm:ss
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%m-%d-%y %H:%M:%S") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%y %H:%M:%S") {
         return Ok(date_time)
     }
     // dd/mm/yy hh:mm:ss
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%d-%m-%y %H:%M:%S") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%y %H:%M:%S") {
         return Ok(date_time)
     }
     // yy/mm/dd hh:mm:ss
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%y-%m-%d %H:%M:%S") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%y-%m-%d %H:%M:%S") {
         return Ok(date_time)
     }
     // mm/dd/yy hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%m-%d-%y %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%y %I:%M %p") {
         return Ok(date_time)
     }
     // dd/mm/yy hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%d-%m-%y %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%y %I:%M %p") {
         return Ok(date_time)
     }
     // yy/mm/dd hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%y-%m-%d %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%y-%m-%d %I:%M %p") {
         return Ok(date_time)
     }
     // yy/dd/mm hh:mm AM
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt.to_ascii_uppercase(), "%y-%d-%m %I:%M %p") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%y-%d-%m %I:%M %p") {
         return Ok(date_time)
     }
 
