347801b710632e2ea5502aeaaa966caf59f1e103
Joshua Becnel <jcbecnel@gmail.com>
2025-03-17 13:22:08 -0500

    fixed datetime issue

--- src/clauses/insert/data_conversion/datetime.rs
+++ src/clauses/insert/data_conversion/datetime.rs
@@ -51,6 +51,39 @@ pub fn datetime_conversion(input: &mut String) -> Result<NaiveDateTime, String> 
     if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%d-%m %I:%M %p") {
         return Ok(date_time)
     }
+    // mm/dd/yyyy hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%Y %H:%M") {
+        return Ok(date_time)
+    }
+    // dd/mm/yyyy hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%Y %H:%M") {
+        return Ok(date_time)
+    }
+    // yyyy/mm/dd hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%m-%d %H:%M") {
+        return Ok(date_time)
+    }
+    // yyyy/dd/mm hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%d-%m %H:%M") {
+        return Ok(date_time)
+    }
+    
+    // mm/dd/yyyy hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%Y %I:%M %p") {
+        return Ok(date_time)
+    }
+    // dd/mm/yyyy hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%d-%m-%Y %I:%M %p") {
+        return Ok(date_time)
+    }
+    // yyyy/mm/dd hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%m-%d %I:%M %p") {
+        return Ok(date_time)
+    }
+    // yyyy/dd/mm hh:mm
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%Y-%d-%m %I:%M %p") {
+        return Ok(date_time)
+    }
     // mm/dd/yy hh:mm:ss AM
     if let Ok(date_time) = NaiveDateTime::parse_from_str(&fmt_dt, "%m-%d-%y %I:%M:%S %p") {
         return Ok(date_time)
