521bc38e22c2ca163b7b88b5effcc7ca13e7be45
jcbecnel <jcbecnel@gmail.com>
2025-12-01 09:49:03 -0600

    Updated conjunction documentation

--- src/query_conjunctions/mod.rs
+++ src/query_conjunctions/mod.rs
@@ -13,378 +13,71 @@ pub enum WhereArg {
     NULL,
 }
 
-/// Trait to add a WHERE clause to a SQL statement.
+/// Trait used for building SQL query conjunctions like WHERE, AND, OR, etc..
 pub trait QueryConjunctions {
-    /// Adds a WHERE clause to your query.
-    /// ```no_run
-    /// let values = WhereArg::Values(vec![
-    ///    SQLDataTypes::Varchar("Q2".to_string()),  SQLDataTypes::Varchar("Q3".to_string())
-    /// ]);
-    /// let data = conn.select("quarterly_earnings", vec!["revenue", "profit"])
-    ///     .where_in("quarter", values)
-    ///     .build()?;
-    /// ```
+    /// Adds a `WHERE` clause to your query.
+    /// The first argument is the column you want to filter on, and the second argument is the values you want to filter by.
+    /// This can be chained with other conjunctions like `and`, `or`, `and_not`, and `or_not`.
+    /// 
+    /// `.where_in(column, values)`
     ///
     /// ```sql
-    /// SELECT revenue, profit FROM quarterly_earning WHERE quarter IN ('Q2', 'Q3');
-    /// ```
-    ///
-    /// ```no_run
-    /// let like = WhereArg::Like("Bob%".to_string());
-    /// let data = conn.delete("employees")
-    ///     .where_in("name", like)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// DELETE FROM employees WHERE name LIKE 'Bob%';
-    /// ```
-    ///
-    /// ```no_run
-    /// let query = WhereArg::Query("SELECT name FROM customers");
-    /// let data = conn.select("employees", vec!["name", "position"])
-    ///     .where_in("name", query)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT name, position FROM employees WHERE name in (SELECT name FROM customers);
-    /// ```
-    ///
-    /// ```no_run
-    /// let null = WhereArg::Null;
-    /// let data = conn.select("employees", vec!["name", "position"])
-    ///     .where_in("hours_worked", null)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT name, position FROM employees WHERE hours_worked IS NULL;
+    /// WHERE column IN (values);
     /// ```
     fn where_in(self, column: &str, values: WhereArg) -> Self;
 
-    /// Adds a WHERE NOT clause to your query.
-    /// ```no_run
-    /// let values = WhereArg::Values(vec![
-    ///    SQLDataTypes::Varchar("Q1".to_string()),  SQLDataTypes::Varchar("Q4".to_string())
-    /// ]);
-    /// let data = conn.select("quarterly_earnings", vec!["revenue", "profit"])
-    ///     .where_not("quarter", values)
-    ///     .build()?;
-    /// ```
+    /// Adds a `WHERE NOT` clause to your query.
+    /// The first argument is the column you want to filter on, and the second argument is the values you want to filter by.
+    /// This can be chained with other conjunctions like `and`, `or`, `and_not`, and `or_not`.
+    /// 
+    /// `.where_not(column, values)`
     ///
     /// ```sql
-    /// SELECT revenue, profit FROM quarterly_earning WHERE quarter NOT IN ('Q2', 'Q3');
+    /// WHERE column NOT IN (values);
     /// ```
-    ///
-    /// ```no_run
-    /// let like = WhereArg::Like("Bob%".to_string());
-    /// let data = conn.select("employees", vec!["name", "position"])
-    ///     .where_not("name", like)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// DELETE FROM employees WHERE name NOT LIKE 'Bob%';
-    /// ```
-    ///
-    /// ```no_run
-    /// let query = WhereArg::Query(
-    ///     "SELECT name FROM customers"
-    /// );
-    /// let data = conn.select("employees", vec!["name", "position"])
-    ///     .where_not("name", query)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT name, position FROM employees WHERE name NOT IN (SELECT name FROM customers);
-    /// ```
-    ///
-    /// ```no_run
-    /// conn.update("employees")
-    ///     .set("hours_worked", 0)
-    ///     .where_not("hours_worked", WhereArg::NULL)
-    ///     .build()?;
-    /// ```
-    /// Is the same as:
-    /// ```sql
-    /// UPDATE employees
-    /// SET hours_worked = 0
-    /// WHERE hours_worked IS NOT NULL;
     fn where_not(self, column: &str, values: WhereArg) -> Self;
 
-    /// Adds an 'AND' to a WHERE clause.
-    /// ```no_run
-    /// let timezones = WhereArg::Values(vec![
-    ///     SQLDataTypes::Varchar("Central European Standard Time"), SQLDataTypes::Varchar("Eastern European Standard Time")
-    /// ]);
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and("timezone", timezones)
-    ///     .build()?;
-    /// ```
+    /// Adds an `AND` conjunction to a WHERE clause.
+    /// The first argument is the column you want to filter on, and the second argument is the values you want to filter by.
+    /// This can be chained with other conjunctions like `or`, `and_not`, and `or_not`.
+    /// 
+    /// `.and(column, values)`
     ///
     /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND timezone IN ('Central European Standard Time', 'Eastern European Standard Time');
-    /// ```
-    ///
-    /// AND LIKE
-    /// ```no_run
-    /// let like = WhereArg::Like("Nor%".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and("country", like)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND country LIKE 'Nor%';
-    /// ```
-    ///
-    /// AND IN (query)
-    /// ```no_run
-    /// let query = WhereArg::Query("SELECT email FROM ban_list".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and("email", query)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND email IN ('SELECT email FROM ban_list');
-    /// ```
-    ///
-    /// AND IS NULL
-    /// ```no_run
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and("phone_number", WhereArg::NULL)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND phone_number IS NULL;
+    /// AND column IN (values);
     /// ```
     fn and(self, column: &str, values: WhereArg) -> Self;
 
-    /// Adds a 'OR' to a WHERE clause.
-    /// ```no_run
-    /// let values = WhereArg::Values(vec!["United States", "Brazil"]);
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .or("country", values)
-    ///     .build()?;
-    /// ```
+    /// Adds an `OR` conjunction to a WHERE clause.
+    /// The first argument is the column you want to filter on, and the second argument is the values you want to filter by.
+    /// This can be chained with other conjunctions like `and`, `and_not`, and `or_not`.
+    /// 
+    /// `.or(column, values)`
     ///
     /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// OR country IN ('United States', 'Brazil');
-    /// ```
-    ///
-    /// OR LIKE
-    /// ```no_run
-    /// let like = WhereArg::Like("United%".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .or("country", like)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// OR country LIKE 'United%';
-    /// ```
-    ///
-    /// OR IN (query)
-    /// ```no_run
-    /// let query = WhereArg::Query("SELECT country FROM countries WHERE population > 100000000".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .or("country", query)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// OR country IN (SELECT country FROM countries WHERE population > 10000000);
-    /// ```
-    ///
-    /// OR IS NULL
-    /// ```no_run
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .or("country", WhereArg::NULL)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// OR country IS NULL;
+    /// OR column IN (values);
     /// ```
     fn or(self, column: &str, values: WhereArg) -> Self;
 
-    /// Adds an 'AND NOT' to a WHERE clause.
-    /// ```no_run
-    /// let values = WhereArg::Values(vec!["France", "Spain"]);
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and_not("country", values)
-    ///     .build()?;
-    /// ```
+    /// Adds an `AND NOT` conjunction to a WHERE clause.
+    /// The first argument is the column you want to filter on, and the second argument is the values you want to filter by.
+    /// This can be chained with other conjunctions like `and`, `or`, and `or_not`.
+    /// 
+    /// `.and_not(column, values)`
     ///
     /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND country NOT IN ('France', 'Spain');
-    /// ```
-    ///
-    /// AND NOT LIKE
-    /// ```no_run
-    /// let like = WhereArg::Like("Nor%".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and_not("country", like)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND country NOT LIKE 'Nor%';
-    /// ```
-    ///
-    /// AND NOT IN (query)
-    /// ```no_run
-    /// let query = WhereArg::Query("SELECT email FROM ban_list".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and_not("email", query)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND email NOT IN ('SELECT email FROM ban_list');
-    /// ```
-    ///
-    /// AND IS NOT NULL
-    /// ```no_run
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .and_not("email", WhereArg::NULL)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// AND email IS NOT NULL;
+    /// AND column NOT IN (values);
     /// ```
     fn and_not(self, column: &str, values: WhereArg) -> Self;
 
-    /// Adds a 'OR NOT' to a WHERE clause.
-    /// ```no_run
-    /// let values = WhereArg::Values(vec!["United States", "Brazil"]);
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_not("continent", europe)
-    ///     .or_not("country", values)
-    ///     .build()?;
-    /// ```
+    /// Adds an `OR NOT` conjunction to a WHERE clause.
+    /// The first argument is the column you want to filter on, and the second argument is the values you want to filter by.
+    /// This can be chained with other conjunctions like `and`, `and_not`, and `or`.
+    /// 
+    /// `.or_not(column, values)`
     ///
     /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent NOT IN ('Europe')
-    /// OR country NOT IN ('United States', 'Brazil');
-    /// ```
-    ///
-    /// OR NOT LIKE
-    /// ```no_run
-    /// let like = WhereArg::Like("United%".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .or_not("country", like)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// OR country NOT LIKE 'United%';
-    /// ```
-    ///
-    /// OR NOT IN (query)
-    /// ```no_run
-    /// let query = WhereArg::Query("SELECT country FROM countries WHERE population > 100000000".to_string());
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_not("continent", europe)
-    ///     .or_not("country", query)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE NOT continent IN ('Europe')
-    /// OR country NOT IN (SELECT country FROM countries WHERE population > 10000000);
-    /// ```
-    ///
-    /// OR NOT NULL
-    /// ```no_run
-    /// let europe = WhereArg::Values(vec![SQLDataTypes::Varchar("Europe")]);
-    /// let data: Vec<Vec<SQLDataTypes>> = conn
-    ///     .select("users", vec!["first_name", "email"])
-    ///     .where_in("continent", europe)
-    ///     .or_not("email", WhereArg::NULL)
-    ///     .build()?;
-    /// ```
-    ///
-    /// ```sql
-    /// SELECT first_name, email FROM users
-    /// WHERE continent IN ('Europe')
-    /// OR email IS NOT NULL;
+    /// OR column NOT IN (values);
     /// ```
     fn or_not(self, column: &str, values: WhereArg) -> Self;
 }
